4ae2652118d6bdaf892bf4ab24fef829f9c87628
jozamo <jozamo@ono.com>
2015-03-17 14:10:04 +0100

    tarea ED

---
+++ DAM_ED04_Actividad/src/dam_ed04_actividad/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dam_ed04_actividad;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cuenta1 miCuenta;
+        double saldoActual;
+
+        miCuenta = new Cuenta1("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        operativa_cuenta(miCuenta, 0);
+    }
+
+    private static void operativa_cuenta(Cuenta1 miCuenta, float cantidad) {
+        try {
+            miCuenta.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            miCuenta.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
